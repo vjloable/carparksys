ee08c9a0a98106f9e018641841aab4458692135a
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-05 01:57:36 +0800

    Completed the Suggestion and Statistics Sections

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -56,67 +56,212 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 children: [
                   Container(
-                      height: 220,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(5)
-                        ),
-                        color: Colors.white,
-                      )
-                  ),
-                  const SizedBox(height: 20, width: double.infinity),
-                  FittedBox(
-                    child: Row(
-                      children: [
-                        Container(
-                            height: 120,
-                            width: 350,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                  Radius.circular(20)
-                              ),
-                              color: Swatch.prime,
-                            ),
-                            child: MaterialButton(
-                              onPressed: (){},
-                              child: const FittedBox(
-                                child: Text(
-                                  'RESERVE',
-                                  style: TextStyle(
-                                    fontSize: 35,
-                                    fontWeight: FontWeight.w400,
-                                    fontFamily: 'Arial'
+                    height: 280,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(20)
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Text('Suggested Parking Space:'),
+                          const SizedBox(
+                            height: 150,
+                            child: FittedBox(
+                                fit: BoxFit.contain,
+                                child: SizedBox(
+                                  width: 300,
+                                  child: Text(
+                                    'W5',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 100
+                                    ),
                                   ),
-                                ),
-                              ),
-                            )
-                        ),
-                        const SizedBox(height: 100, width: 20),
-                        Container(
-                            height: 120,
-                            width: 120,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                  Radius.circular(20)
-                              ),
-                              color: Swatch.prime,
-                            )
-                        )
-                      ],
-                    )
+                                )
+                            ),
+                          ),
+                          const SizedBox(width: 250, child: Divider(color: Swatch.prime, thickness: 1)),
+                          const SizedBox(height: 1, width: 15),
+                          FittedBox(
+                              child: Row(
+                                children: [
+                                  Container(
+                                      height: 60,
+                                      width: 200,
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(20)
+                                        ),
+                                        color: Swatch.prime,
+                                      ),
+                                      child: ElevatedButton(
+                                         style: ButtonStyle(
+                                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                               RoundedRectangleBorder(
+                                                   borderRadius: BorderRadius.circular(18.0)
+                                               )
+                                           )
+                                         ),
+                                          onPressed: (){},
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: const [
+                                              Icon(Icons.confirmation_number_outlined, size: 24),
+                                              Text(
+                                                '  RESERVE',
+                                                textAlign: TextAlign.start,
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w400,
+                                                  fontFamily: 'Arial',
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                      )
+                                  ),
+                                  const SizedBox(height: 1, width: 15),
+                                  Container(
+                                      height: 60,
+                                      width: 120,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(20)
+                                        ),
+                                        color: Swatch.buttons.shade400,
+                                      ),
+                                      child: ElevatedButton(
+                                          style: ButtonStyle(
+                                              backgroundColor: MaterialStateProperty.all<Color>(Swatch.buttons.shade400),
+                                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                                  RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(18.0)
+                                                  )
+                                              )
+                                          ),
+                                          onPressed: (){},
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: const [
+                                              Icon(
+                                                Icons.space_dashboard_outlined,
+                                                size: 24,
+                                                color: Swatch.prime,
+                                              ),
+                                              Text(
+                                                '  LOTS',
+                                                textAlign: TextAlign.start,
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w400,
+                                                  fontFamily: 'Arial',
+                                                  color: Swatch.prime
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                      )
+                                  )
+                                ],
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
+                  ///const SizedBox(height: 20, width: double.infinity),
                   const SizedBox(height: 40, width: double.infinity),
                   Container(
-                    height: 960,
+                    height: 150,
                     width: double.infinity,
                     decoration: const BoxDecoration(
                         borderRadius: BorderRadius.all(
-                            Radius.circular(20)
+                            Radius.circular(30)
                         ),
                         color: Colors.white,
                     ),
-                    child: const SizedBox(width: 200, height: 200),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Parking Space Statistics:\n'),
+                          const SizedBox(width: 250, child: Divider(color: Swatch.prime, thickness: 1)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                      '12',
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                  Text(
+                                      'OCCUPIED',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                      '10',
+                                      style: TextStyle(
+                                          fontSize: 40,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                  Text(
+                                      'AVAILABLE',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                      '8',
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                  Text(
+                                      'RESERVED',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                          color: Swatch.buttons.shade800
+                                      )
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ]
+                    )
                   )
                 ],
               ),
@@ -127,7 +272,7 @@ class _HomePageState extends State<HomePage> {
         height: 80,
         child:
           BottomAppBar(
-              elevation: 50,
+              elevation: 10,
               shape: const CircularNotchedRectangle(),
               notchMargin: 8,
               color: Colors.white,
@@ -135,7 +280,7 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       IconButton(
                           icon: const Icon(Icons.home),
@@ -154,7 +299,7 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                   Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       IconButton(
                           icon: const Icon(Icons.space_dashboard_rounded),
@@ -173,8 +318,7 @@ class _HomePageState extends State<HomePage> {
           height: 70,
           width: 70,
           child: FloatingActionButton(
-            tooltip: 'Ticket',
-            elevation: 3,
+            elevation: 0,
             onPressed: () {
               /*
               Navigator.push(
@@ -183,13 +327,9 @@ class _HomePageState extends State<HomePage> {
               );
               */
             },
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+            child:
                   Icon(Icons.confirmation_num, size: 32, color: Swatch.buttons.shade500),
-                ]
-            ),
-          )
+          ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
