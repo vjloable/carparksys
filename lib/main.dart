be4fa06b1bfff53a18598c9f8c8e38e81569e890
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-06 11:41:40 +0800

    Routed the Ticket QR Page to the FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:carparksys/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'assets/swatches/custom_colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
