5d3ebd48114c1456f01734d68726b35239874d23
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-12 18:41:36 +0800

    Added Google Sign In authentication service

---
+++ lib/services/auth.dart
@@ -0,0 +1,41 @@
+import 'package:carparksys/pages/home.dart';
+import 'package:carparksys/pages/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService{
+
+  handleAuthState() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData){
+          return const HomePage();
+        } else {
+          return const LoginPage();
+        }
+      }
+    );
+  }
+
+  signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+      scopes: <String>['email']
+    ).signIn();
+
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+  
+}
