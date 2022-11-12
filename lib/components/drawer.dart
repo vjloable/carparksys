309bb217e49c6df665ad1876347b51d7300ea9df
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-13 07:42:44 +0800

    Routed showModalBottomSheet of Ticket; Code Cleanup

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,6 +1,6 @@
-import 'package:carparksys/services/auth.dart';
+import 'package:carparksys/components/ticket.dart';
+import 'package:carparksys/services/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../../assets/swatches/custom_colors.dart';
@@ -10,8 +10,9 @@ Widget drawerHeader = UserAccountsDrawerHeader(
   accountName: Text(FirebaseAuth.instance.currentUser!.displayName!, style: TextStyle(fontFamily: 'Arial', color: Swatch.buttons.shade800)),
   accountEmail: Text(FirebaseAuth.instance.currentUser!.email!, style: TextStyle(fontFamily: 'Arial', color: Swatch.buttons.shade800)),
   currentAccountPicture: CircleAvatar(
-    backgroundImage: NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!)
-    //Icon(Icons.account_circle, size: 70, color: Swatch.buttons.shade400),
+    backgroundImage: NetworkImage(
+        FirebaseAuth.instance.currentUser!.photoURL!,
+    )
   ),
 );
 
@@ -28,13 +29,34 @@ Widget drawerItems(BuildContext context, int numPops) {
       ListTile(
         title: const Text('My Ticket'),
         leading: const Icon(Icons.confirmation_num_rounded),
-        onTap: () {},
+        onTap: () {
+          if (numPops == 2) {
+            Navigator.of(context).pop();
+          } else if (numPops == 3) {
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+          }
+          showModalBottomSheet(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              context: context,
+              builder: (BuildContext context) {
+                return showTicket(context);
+              }
+          );
+        },
       ),
       ListTile(
         title: const Text('Lots'),
         leading: const Icon(Icons.dashboard_rounded),
         onTap: () {
-          Navigator.of(context)..pop()..push(MaterialPageRoute(builder: (context) => const LotsPage()),);
+          Navigator.of(context).pop();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const LotsPage()),
+          );
         },
       ),
       ListTile(
@@ -52,17 +74,8 @@ Widget drawerItems(BuildContext context, int numPops) {
         title: const Text('Sign out'),
         leading: const Icon(Icons.logout_rounded),
         onTap: () {
+          Navigator.of(context).popUntil((route) => route.isFirst);
           AuthService().signOut();
-          // switch(numPops){
-          //   case 2: {
-          //     Navigator.of(context)..pop()..pop();
-          //   }
-          //   break;
-          //
-          //   case 3: {
-          //     Navigator.of(context)..pop()..pop()..pop();
-          //   }
-          // }
         },
       ),
     ],
