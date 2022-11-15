3e6d758afecf8506bd3fdf213e7a0b51d3a24ff1
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-15 12:32:10 +0800

    Initialized suggestion logic

---
+++ lib/controllers/suggestion.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:carparksys/services/rtdb.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class SuggestionController{
+  final _database = RTDBService().database;
+  late String _prevSuggestion = '...';
+  late StreamSubscription _suggestionStream;
+  late StreamController<List> suggestionStreamController = StreamController<List>();
+
+  void activateListenersSuggestion() {
+    _suggestionStream = _database.child('spaces/').onValue.listen((event) {
+      List<String> suggestions = [];
+      Iterable<DataSnapshot> snapshot = event.snapshot.children;
+      List<String> lots = snapshot.map((e) => e.key as String).toList();
+      List<int> status = snapshot.map((e) => e.value as int).toList();
+
+      for(int i = 0; i < lots.length; i++){
+        if(status[i] == 1) {
+          suggestions.add(lots[i].toString());
+        }
+      }
+
+      if(_prevSuggestion == '...'){
+        suggestionStreamController.add(suggestions);
+      }else if(suggestions.contains(_prevSuggestion)){
+        suggestions = [];
+        suggestions.add(_prevSuggestion);
+        suggestionStreamController.add(suggestions);
+      }else{
+        suggestionStreamController.add(suggestions);
+      }
+    });
+  }
+
+  void setPrevSuggestion(String lot){
+    _prevSuggestion = lot;
+  }
+
+  void deactivateListenerSuggestion() {
+    _suggestionStream.cancel();
+  }
+
+}
