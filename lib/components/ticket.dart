61b9903042a717bfa818f2055198746d7c1d1db0
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-19 00:23:06 +0800

    Fixed ticket box with adaptive dimensions

--- lib/components/ticket.dart
+++ lib/components/ticket.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
 
 Widget showTicket(BuildContext context){
-  return Stack(
-    children: [
-      Positioned(
-        top: 20,
-        left: 10,
-        child: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back, size: 30),
-          splashRadius: 1,
-        ),
-      ),
-      SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Container(
-                  color: Colors.grey,
-                  height: 300,
-                  width: 300,
-                ),
-              )
-            ],
+  return SizedBox(
+    height: 400,
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 20,
+          left: 10,
+          child: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back, size: 30),
+            splashRadius: 1,
           ),
+        ),
+        SizedBox(
+          height: 300,
+          width: 300,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.grey,
+                  height: 200,
+                  width: 300,
+                )
+              ],
+            ),
+          )
         )
-      )
-    ],
+      ],
+    ),
   );
 }
