dc7416f103d3ec064b808c8ea62afa8eb2c42b76
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-19 00:19:47 +0800

    Fixed Firebase RT Database persistent offline data sync queue

--- lib/components/reservation.dart
+++ lib/components/reservation.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:carparksys/assets/swatches/custom_colors.dart';
 import 'package:carparksys/components/time_runner.dart';
 import 'package:carparksys/controllers/reserve.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:slide_to_act/slide_to_act.dart';
 
@@ -17,19 +19,19 @@ class ReserveLot extends StatefulWidget {
 }
 
 class _ReserveLotState extends State<ReserveLot> {
-  late List<String> _parkingLotsName = [];
-  late List<int> _parkingLotsStatus = [];
-  late String _time = '';
   SpacesController controllerSpaces = SpacesController();
-  late Stream<List> spacesStream = controllerSpaces.spacesStreamController.stream;
-  late StreamSubscription<List> spacesStreamSubscription;
-  late Timer timer;
-
+  ConnectivityResult connectivityResult = ConnectivityResult.none;
+  int isPassed = 2;
+  final Connectivity _connectivity = Connectivity();
   final Widget _errorIcon = const Icon(Icons.error, color: SigCol.red);
   final Widget _passedIcon = const Icon(Icons.check_circle, color: SigCol.green);
-  final Widget _progBar = const Icon(Icons.access_time_filled_outlined, color: SigCol.orange);
-  
-  int isPassed = 2;
+  final Widget _waitingIcon = const Icon(Icons.access_time_filled_outlined, color: SigCol.orange);
+  late StreamSubscription<ConnectivityResult> connectivitySubscription;
+  late StreamSubscription<List> spacesStreamSubscription;
+  late List<String> _parkingLotsName = [];
+  late Timer timer;
+  late String _time = '';
+  late bool _connectionResult = true;
 
   @override
   void initState() {
@@ -38,6 +40,7 @@ class _ReserveLotState extends State<ReserveLot> {
     timer = Timer.periodic(const Duration(seconds: 1), (Timer t) => updateTime());
     controllerSpaces.activateListenersSpaces();
     spacesStreamListener();
+    connectivityStreamListener();
   }
 
   void updateTime() {
@@ -47,18 +50,52 @@ class _ReserveLotState extends State<ReserveLot> {
   }
 
   void spacesStreamListener() {
-    spacesStreamSubscription = spacesStream.listen((event){
+    spacesStreamSubscription = controllerSpaces.spacesStreamController.stream.listen((event){
       setState(() {
         _parkingLotsName = event[0];
-        _parkingLotsStatus = event[1];
       });
     });
   }
 
-  void updateSubmitIcon() {
-    setState(() {
-      isPassed = Reserve().stateCheck;
-    });
+  void connectivityStreamListener() {
+    connectivitySubscription = _connectivity.onConnectivityChanged.listen(
+        _updateConnectionStatus
+    );
+  }
+
+  void updateSubmitIcon(int overrider) {
+    if(mounted){
+      setState(() {
+        isPassed = (overrider == 0) ? Reserve().stateCheck : 3;
+      });
+    }
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    var reliabilityCheck = false;
+    if(result != ConnectivityResult.none){
+      try {
+        final result = await InternetAddress.lookup('google.com');
+        final result2 = await InternetAddress.lookup('facebook.com');
+        final result3 = await InternetAddress.lookup('microsoft.com');
+        if ((result.isNotEmpty && result[0].rawAddress.isNotEmpty) ||
+            (result2.isNotEmpty && result2[0].rawAddress.isNotEmpty) ||
+            (result3.isNotEmpty && result3[0].rawAddress.isNotEmpty)) {
+          reliabilityCheck = true;
+        } else {
+          reliabilityCheck = false;
+        }
+      } on SocketException catch (_) {
+        reliabilityCheck = false;
+      }
+      setState((){
+        _connectionResult = reliabilityCheck;
+      });
+    }else{
+      setState((){
+        _connectionResult = reliabilityCheck;
+      });
+    }
   }
 
   @override
@@ -77,106 +114,148 @@ class _ReserveLotState extends State<ReserveLot> {
             ),
           ),
           SizedBox(
-            height: 320,
+            height: 400,
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: SizedBox(
-                      height: 28,
-                      width: 300,
-                      child: Text(
-                        'Do you confirm this reservation?',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Theme.of(context).colorScheme.onPrimary
-                        ),
-                      ),
-                    ),
+              child: Visibility(
+                visible: _connectionResult,
+                replacement: Material(
+                  elevation: 10,
+                  borderRadius: const BorderRadius.all(
+                      Radius.circular(50)
                   ),
-                  Material(
-                    elevation: 10,
-                    borderRadius: const BorderRadius.all(
-                        Radius.circular(10)
-                    ),
-                    color: Swatch.prime,
-                    child: SizedBox(
-                      height: 160,
-                      width: 340,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                  color: Swatch.prime,
+                  child: Container(
+                    height: 260,
+                    width: 330,
+                    padding: const EdgeInsets.all(40),
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const SizedBox(width: 5),
-                          SizedBox(
-                            width: 100,
-                            height: 100,
-                            child: Material(
-                              elevation: 3,
-                              color: Theme.of(context).colorScheme.background,
-                              child: Container(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  widget._lot,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 60,
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context).colorScheme.onPrimary
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 100, width: 1, child: VerticalDivider(color: Swatch.buttons.shade600, thickness: 1)),
-                          SizedBox(
-                            width: 140,
-                            child: Text(
-                              'Your Parking Space',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w400,
-                                  color: Swatch.buttons.shade800
-                              ),
-                            ),
-                          ),
+                          CircleAvatar(radius: 70, backgroundColor: Theme.of(context).colorScheme.onPrimary, child: const Icon(Icons.wifi_off, color: SigCol.red, size: 60)),
+                          const SizedBox(height: 20),
+                          const Text('Connection Error!', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+                          const SizedBox(height: 30),
+                          const Text('Slow or no Internet connection.', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Arial', fontWeight: FontWeight.w300, fontSize: 30)),
+                          const SizedBox(height: 10),
+                          const Text('Check your connection, then try again', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Arial', fontWeight: FontWeight.w300, fontSize: 26)),
                         ],
                       ),
                     ),
                   ),
-                  const SizedBox(width: 1, height: 10),
-                  SizedBox(
-                      height: 55,
-                      width: 340,
-                      child: SlideAction(
-                        sliderButtonIconSize: 20,
-                        sliderButtonIconPadding: 10,
-                        innerColor: Swatch.prime,
-                        outerColor: Swatch.buttons.shade900,
-                        elevation: 15,
-                        text: '   Slide to confirm',
-                        submittedIcon: isPassed == 2 ? _progBar : isPassed == 1 ? _passedIcon : _errorIcon,
-                        textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w100, color: Swatch.prime.shade100),
-                        onSubmit: () async {
-                          await Reserve().reserve(widget._lot);
-                          Future.delayed(const Duration(seconds: 1));
-                          updateSubmitIcon();
-                          Future.delayed(
-                              const Duration(milliseconds: 200),
-                                  () => Navigator.pop(context)
-                          );
-                        },
-                      )
+                ),
+                child: SizedBox(
+                  height: 300,
+                  width: 330,
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 20),
+                        FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: SizedBox(
+                            height: 28,
+                            width: 300,
+                            child: Text(
+                              'Do you confirm this reservation?',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Theme.of(context).colorScheme.onPrimary
+                              ),
+                            ),
+                          ),
+                        ),
+                        Material(
+                          elevation: 10,
+                          borderRadius: const BorderRadius.all(
+                              Radius.circular(10)
+                          ),
+                          color: Swatch.prime,
+                          child: SizedBox(
+                            height: 160,
+                            width: 340,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const SizedBox(width: 5),
+                                SizedBox(
+                                  width: 100,
+                                  height: 100,
+                                  child: Material(
+                                    elevation: 3,
+                                    color: Theme.of(context).colorScheme.background,
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        widget._lot,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 60,
+                                            fontWeight: FontWeight.bold,
+                                            color: Theme.of(context).colorScheme.onPrimary
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 100, width: 1, child: VerticalDivider(color: Swatch.buttons.shade600, thickness: 1)),
+                                SizedBox(
+                                  width: 140,
+                                  child: Text(
+                                    'Your Parking Space',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w400,
+                                        color: Swatch.buttons.shade800
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 1, height: 10),
+                        SizedBox(
+                            height: 55,
+                            width: 340,
+                            child: SlideAction(
+                              sliderButtonIconSize: 20,
+                              sliderButtonIconPadding: 10,
+                              innerColor: Swatch.prime,
+                              outerColor: Swatch.buttons.shade900,
+                              elevation: 15,
+                              text: '   Slide to confirm',
+                              submittedIcon: isPassed == 2 ? _waitingIcon : isPassed == 1 ? _passedIcon : _errorIcon,
+                              textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w100, color: Swatch.prime.shade100),
+                              onSubmit: () async {
+                                if (!_connectionResult){
+                                  await Reserve().reserve(widget._lot);
+                                  Future.delayed(const Duration(seconds: 1));
+                                  updateSubmitIcon(0);
+                                }else{
+                                  updateSubmitIcon(3);
+                                }
+
+                                Future.delayed(
+                                    const Duration(milliseconds: 200),
+                                        () => mounted ? Navigator.pop(context) : null
+                                );
+                              },
+                            )
+                        ),
+                        const SizedBox(width: 1, height: 20)
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 1, height: 20)
-                ],
+                ),
               ),
             ),
           ),
@@ -187,6 +266,7 @@ class _ReserveLotState extends State<ReserveLot> {
   @override
   void deactivate() {
     timer.cancel();
+    connectivitySubscription.cancel();
     spacesStreamSubscription.cancel();
     controllerSpaces.deactivateListenerSpaces();
     super.deactivate();
