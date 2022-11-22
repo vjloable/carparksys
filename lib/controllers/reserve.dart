4fcd5b65295ae47b73fbef8465dfc3e0391cbd59
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-22 12:27:51 +0800

    Added checks to database for the ticketing functionality

--- lib/controllers/reserve.dart
+++ lib/controllers/reserve.dart
@@ -1,22 +1,64 @@
 import 'package:carparksys/services/rtdb.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-RTDBService rtdbRef = RTDBService();
+RTDBService rtdb = RTDBService();
 
 class Reserve{
   late DataSnapshot _snapshotCheck;
-  late int stateCheck = 1;
+  late DataSnapshot _snapshotUser;
+  static int stateCheck = 0;
+
+  int getStateCheck(){
+    return stateCheck;
+  }
+
+  void updateStateCheck(int state){
+    stateCheck = state;
+  }
 
   Future<void> reserve(lot) async {
-    _snapshotCheck = await rtdbRef.databaseRef.child('spaces/$lot').get();
+    _snapshotCheck = await rtdb.databaseRef.child('spaces/$lot').get();
     if(_snapshotCheck.value != 1){
-      stateCheck = 0;
+      updateStateCheck(3);
     }else {
-      await rtdbRef.databaseRef.update(
-          {
-            'spaces/$lot': 3,
-          }
-      );
+      var userId = FirebaseAuth.instance.currentUser?.uid;
+      _snapshotUser = await rtdb.databaseRef.child('users/$userId').get();
+      if(_snapshotUser.exists){
+        if(_snapshotUser.child('has_ticket').value == false) {
+          await rtdb.databaseRef.update(
+              {
+                'users/$userId' : {
+                  'has_ticket' : true,
+                  'lot' : {
+                    '$lot' : 3,
+                  }
+                }
+              });
+          await rtdb.databaseRef.update(
+              {
+                'spaces/$lot': 3,
+              });
+          updateStateCheck(2);
+        }else{
+          //Already reserved
+          updateStateCheck(3);
+        }
+      }else{
+        await rtdb.databaseRef.update(
+            {
+              'users/$userId' : {
+                'has_ticket' : true,
+                'lot' : 3,
+              }
+            });
+        await rtdb.databaseRef.update(
+            {
+              'spaces/$lot': 3,
+            });
+        updateStateCheck(1);
+      }
     }
+    print(stateCheck);
   }
 }
