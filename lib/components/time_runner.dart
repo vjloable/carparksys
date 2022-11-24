bab9e2880335b0a98e56a8ebb20783b8b4607bfa
Eagl3ye <vincejaphethloable@gmail.com>
2022-11-24 22:19:53 +0800

    Updated time_runner

--- lib/components/time_runner.dart
+++ lib/components/time_runner.dart
@@ -5,10 +5,6 @@ class TimeRunner{
     return DateFormat('MM/dd/yyyy').add_jms().format(dateTime);
   }
 
-  int countdown(int seconds){
-    return DateTime.now().millisecondsSinceEpoch + 1000 * seconds;
-  }
-
   DateTime now(){
     return DateTime.now();
   }
